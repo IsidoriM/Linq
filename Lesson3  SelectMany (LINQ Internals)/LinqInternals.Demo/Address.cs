bbd8b424b9f7da3fb7aae2fd7fabedb96b3c9342
IsidoriM <isidorimassimiliano2@gmail.com>
2025-03-10 11:28:15 +0100

    Lesson3 SelectMany (LINQ Internals)

---
+++ Lesson3  SelectMany (LINQ Internals)/LinqInternals.Demo/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace LinqInternals.Demo
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Lesson3  SelectMany (LINQ Internals)/LinqInternals.Demo/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace LinqInternals.Demo
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Phone[] Phones { get; set; }
+    }
+}
